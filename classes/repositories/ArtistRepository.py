c37f0df8903d8b0d2b6e2787ee8a854e7dce1736
Ben Willis <cw003504@live.reading.ac.uk>
2023-05-11 21:45:29 +0100

    REFAC: Add comments to artist repository

--- classes/repositories/ArtistRepository.py
+++ classes/repositories/ArtistRepository.py
@@ -4,6 +4,7 @@ from classes.models.Database import Database
 
 
 class ArtistRepository(Database):
+    # Get an artist from the database with a given artist_id
     def get_artist_by_id(self, artist_id:int):
         sql = 'SELECT * FROM public."Artist" WHERE artist_id = %s;' # Note: no quotes
         data = (artist_id, )
@@ -11,6 +12,7 @@ class ArtistRepository(Database):
         user = Artist(row[0],row[1],row[2])
         return user.to_json()
     
+    # Get all artists with a given genre_id
     def get_artists_by_genre_id(self, genre_id:int):
         sql = 'SELECT * FROM public."Artist" WHERE "genre_id" = %s;' # Note: no quotes
         data = (genre_id, )
@@ -20,6 +22,8 @@ class ArtistRepository(Database):
             artist = Artist(rows[x][0],rows[x][1],rows[x][2])
             res.append(artist)
         return jsonpickle.encode(res, False)
+    
+    # Get all artists in the database
     def get_all_artists(self):
         sql = 'SELECT * FROM public."Artist";' # Note: no quotes
         data = ()

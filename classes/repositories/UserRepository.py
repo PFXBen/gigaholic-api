317d9834e2743363d1b9143216a40bd883bd2f67
Ben Willis <cw003504@live.reading.ac.uk>
2023-05-12 17:43:18 +0100

    FEAT: Add Concert and ConcertRepository

--- classes/repositories/UserRepository.py
+++ classes/repositories/UserRepository.py
@@ -3,6 +3,7 @@ from classes.models.User import User
 
 
 class UserRepository(Database):
+     # Get a user from the database with a given user_id
     def get_user_by_id(self, user_id:int):
         sql = 'SELECT * FROM public."Users" WHERE user_id = %s;' # Note: no quotes
         data = (user_id, )
@@ -10,7 +11,7 @@ class UserRepository(Database):
         print(row)
         user = User(row[0],row[1],row[2],row[3],row[4])
         return user.to_json()
-    
+     # Get a user from the database with a given UserName
     def get_user_by_username(self, user_name:str):
         sql = 'SELECT * FROM public."Users" WHERE "UserName" = %s;' # Note: no quotes
         data = (user_name, )

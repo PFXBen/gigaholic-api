17628b6c442d53c80ca28f3f8e59eae00ec14bd8
Ben Willis <cw003504@live.reading.ac.uk>
2023-05-12 21:44:03 +0100

    FEAT: Extend hash generator test script to check password match aswell

--- password_hash_generator.py
+++ password_hash_generator.py
@@ -6,4 +6,7 @@ password_to_hash = sys.argv[0]
 
 hashed = PasswordHelper.get_hashed_password(password_to_hash)
 
+is_match = PasswordHelper.check_password(sys.argv[0],hashed)
+
 print(hashed)
+print(is_match)
